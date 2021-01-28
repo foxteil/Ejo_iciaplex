bf524ff9636bac19a91d11d30990a036cab85fa1
yjhh1 <yjhh1@DESKTOP-DEOMJ6D>
2021-01-28 11:05:12 +0900

    프로젝트 PUSH 21/01/28 _승호

--- iciaplex/src/main/java/icia/kotlin/plex/HomeController.java
+++ iciaplex/src/main/java/icia/kotlin/plex/HomeController.java
@@ -10,20 +10,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
-/**
- * Handles requests for the application home page.
- */
+
 @Controller
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	public ModelAndView home(Locale locale, ModelAndView mv) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
 		Date date = new Date();
@@ -31,9 +27,12 @@ public class HomeController {
 		
 		String formattedDate = dateFormat.format(date);
 		
-		model.addAttribute("serverTime", formattedDate );
+		mv.addObject("welcome","스프링에 온것을 환영하다");
+		mv.addObject("serverTime", formattedDate );
 		
-		return "home";
+		
+		
+		return mv;
 	}
 	
 }
