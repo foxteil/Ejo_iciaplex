bff7fe84806e21565a74c2cbca5612ef83c21f20
yjhh1 <yjhh1@DESKTOP-DEOMJ6D>
2021-01-28 16:36:03 +0900

    foxtail commit

--- iciaplex/src/main/java/icia/kotlin/plex/HomeController.java
+++ iciaplex/src/main/java/icia/kotlin/plex/HomeController.java
@@ -8,8 +8,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 
@@ -43,9 +45,15 @@ public class HomeController {
 		return mav;
 	}
 
-	@RequestMapping(value="/LogIn", method= {RequestMethod.GET, RequestMethod.POST})
-	public ModelAndView logIn() {
+	@RequestMapping(value="/LogIn", method= { RequestMethod.POST})
+	public ModelAndView logIn(@ModelAttribute Member m, @RequestParam("memberInfo") String [] mInfo) {
 		ModelAndView mav = new ModelAndView();
+		
+		mav.addObject("mId", m.getMId());
+		mav.addObject("mPwd", m.getMPwd());
+		mav.addObject("mInfoId", mInfo[0]);
+		mav.addObject("mInfoPwd", mInfo[1]);
+		
 		mav.setViewName("loginForm");		
 		
 		return mav;
--- iciaplex/src/main/webapp/WEB-INF/views/loginForm.jsp
+++ iciaplex/src/main/webapp/WEB-INF/views/loginForm.jsp
@@ -7,20 +7,28 @@
 <title>LoginForm</title>
 </head>
 <body>
-<input type="text" name="memberInfo" placeholder="아이디를 입력해주세요"/>
-<input type="password" name ="memberInfo" placeholder="비밀번호를 입력해주세여"/>
+<input type="text" name="mId" placeholder="아이디를 입력해주세요"/>
+<input type="password" name ="mPwd" placeholder="비밀번호를 입력해주세여"/>
 <input type="button" value="서버요청" onclick="moveLogIn()" >
-
+<br />
+${mId }
+${mPwd }<br />
+${mInfoId }
+${mInfoPwd }
+	
+	
 </body>
 <script>
 function moveLogIn(){
-	var memberInfo = document.getElementsByName("memberInfo");
+	var mId = document.getElementsByName("mId")[0];
+	var mPwd = document.getElementsByName("mPwd")[0];
 	
 	var form = document.createElement("form");
-	form.action = "LogIn";
+	form.action = "LogIn?memberInfo="+mId.value+"&memberInfo="+mPwd.value;
 	form.method = "post";
 	
-	form.appendChild(memberInfo);
+	form.appendChild(mId);
+	form.appendChild(mPwd);
 	
 	document.body.appendChild(form);
 	
